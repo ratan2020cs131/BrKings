97ee0cffce03339ba8e514bb13cf7ef08bcfc5b5
Anmol Shukla <anmol.28422@gmail.com>
2023-09-13 11:38:01 +0530

    create Product API

---
+++ server/controllers/productController.js
@@ -0,0 +1,49 @@
+import productModel from "../models/productModel.js";
+import fs from "fs";
+import slugify from "slugify";
+
+export const createProductController = async (req, res) => {
+  try {
+    const { name, slug, description, price, category, quantity, shipping } =
+      req.fields;
+    const { photo } = req.files;
+    if (!name) {
+      return res.status(500).send({ error: "Name is required" });
+    }
+    if (!description) {
+      return res.status(500).send({ error: "Description is required" });
+    }
+    if (!price) {
+      return res.status(500).send({ error: "Price is required" });
+    }
+    if (!category) {
+      return res.status(500).send({ error: "Category is required" });
+    }
+    if (!quantity) {
+      return res.status(500).send({ error: "Quantity is required" });
+    }
+    if (photo && photo.size > 2000000) {
+      return res
+        .status(500)
+        .send({ error: "Photo is required and should be less than 2MB" });
+    }
+    const products = new productModel({ ...req.fields, slug: slugify(name) });
+    if (photo) {
+      products.photo.data = fs.readFileSync(photo.path);
+      products.photo.contentType = photo.type;
+    }
+    await products.save();
+    res.status(201).send({
+      success: true,
+      message: "Product Created Successfully",
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error in creating product",
+    });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ app.use(express.json());
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
 
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to Brownie Kings</h1>");
